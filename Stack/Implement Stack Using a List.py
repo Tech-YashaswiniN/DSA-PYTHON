befeaa379c913d6b859c59c8af81cb700204845c
Tech-YashaswiniN <yashawininanjappajigithub@gmail.com>
2025-02-05 16:19:14 +0530

    Implement Stack Using a List

---
+++ Stack/Implement Stack Using a List.py
@@ -0,0 +1,32 @@
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+    
+    def push(self,item):
+        self.stack_list.append(item)
+
+    def is_empty(self):
+        return len(self.stack_list) == 0
+    
+    def pop(self):
+        if not self.is_empty():
+            return self.stack_list.pop()
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.stack_list[-1]
+    
+    def size(self):
+        return len(self.stack_list)
+
+stack = Stack()
+stack.push(1)
+stack.push(2)
+stack.push(3)
+stack.push(4)
+stack.push(5)
+
+print("Is empty: ",stack.is_empty())
+print("Top element is: ",stack.peek())
+print("The size of the stack of list is: ",stack.size())
+print("The item poped is: ", stack.pop())
